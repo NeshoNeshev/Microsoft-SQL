0af4f90168509cefc1e87583c2a430569a31e9c4
Nesho Neshev <neshev1978@gmail.com>
2021-01-18 15:11:34 +0200

    Update Problem 5.Online Store Database.sql

--- TableRelations/Problem 5.Online Store Database.sql
+++ TableRelations/Problem 5.Online Store Database.sql
@@ -1,7 +1,21 @@
 CREATE DATABASE OnlineStore
 
-USE OnlineStore
+CREATE TABLE Cities
+(
+    CityID INT IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL
+)
+CREATE TABLE Customers
+(
+    CustomerID INT IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    Birthday DATE,
+    CityID INT NOT NULL,
 
+    CONSTRAINT FK_Customers_Cities
+        FOREIGN KEY (CityID)
+            REFERENCES Cities (CityID)
+)
 CREATE TABLE Orders
 (
     OrderID    INT IDENTITY PRIMARY KEY,
@@ -11,25 +25,22 @@ CREATE TABLE Orders
         FOREIGN KEY (CustomerID)
             REFERENCES Customers (CustomerID)
 )
-
-CREATE TABLE Customers
+CREATE TABLE ItemTypes
 (
-    CustomerID INT IDENTITY PRIMARY KEY,
-    [Name] VARCHAR(50) NOT NULL,
-    Birthday DATE,
-    CityID INT NOT NULL,
-
-    CONSTRAINT FK_Customers_Cities
-        FOREIGN KEY (CityID)
-            REFERENCES Cities (CityID)
+    ItemTypeID INT IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL
 )
 
-CREATE TABLE Cities
+CREATE TABLE Items
 (
-    CityID INT IDENTITY PRIMARY KEY,
-    [Name] VARCHAR(50) NOT NULL
-)
+    ItemID INT IDENTITY PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    ItemTypeID INT NOT NULL,
 
+    CONSTRAINT FK_Items_ItemTypes
+        FOREIGN KEY (ItemTypeID)
+            REFERENCES ItemTypes (ItemTypeID)
+)
 CREATE TABLE OrderItems
 (
     OrderID INT IDENTITY PRIMARY KEY,
@@ -42,21 +53,4 @@ CREATE TABLE OrderItems
     CONSTRAINT FK_OrderItems_Items
         FOREIGN KEY (ItemID)
             REFERENCES Items (ItemID)
-)
-
-CREATE TABLE Items
-(
-    ItemID INT IDENTITY PRIMARY KEY,
-    [Name] VARCHAR(50) NOT NULL,
-    ItemTypeID INT NOT NULL,
-
-    CONSTRAINT FK_Items_ItemTypes
-        FOREIGN KEY (ItemTypeID)
-            REFERENCES ItemTypes (ItemTypeID)
-)
-
-CREATE TABLE ItemTypes
-(
-    ItemTypeID INT IDENTITY PRIMARY KEY,
-    [Name] VARCHAR(50) NOT NULL
 )
