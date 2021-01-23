10d2afb4acf6dc82f9fb7855c1932705c1668df2
Nesho Neshev <neshev1978@gmail.com>
2021-01-23 13:58:17 +0200

    Update Problem 15.User Email Providers.sql

--- BuildInFunctions/Problem 15.User Email Providers.sql
+++ BuildInFunctions/Problem 15.User Email Providers.sql
@@ -1,5 +1,3 @@
 SELECT [Username],SUBSTRING(Email,CHARINDEX('@',EMAIL)+1,LEN(Email))
 	AS [Email Provider] FROM Users
-      ORDER BY [Email Provider]
-      
-  
+      ORDER BY [Email Provider] ASC, Username ASC
