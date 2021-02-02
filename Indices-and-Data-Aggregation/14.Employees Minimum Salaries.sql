647a5f26425fddf770111cd66da7efca22d2be35
Nesho Neshev <neshev1978@gmail.com>
2021-02-02 08:19:53 +0200

    Update 14.Employees Minimum Salaries.sql

--- Indices-and-Data-Aggregation/14.Employees Minimum Salaries.sql
+++ Indices-and-Data-Aggregation/14.Employees Minimum Salaries.sql
@@ -1,4 +1,6 @@
-SELECT d.DepartmentID,MIN(e.Salary) AS [TotalSalary] FROM Departments AS d
-	JOIN Employees AS e ON e.DepartmentID = d.DepartmentID
-		WHERE d.DepartmentID IN(2,5,7) AND e.HireDate >'01/01/2000'
-			GROUP BY d.DepartmentID
+SELECT e.DepartmentID, 
+       MIN(Salary) AS MinimumSalary
+    		FROM Employees AS e
+   			 WHERE e.DepartmentID IN (2, 5, 7)
+      				AND e.HireDate > '01-01-2000'
+    					GROUP BY e.DepartmentID
