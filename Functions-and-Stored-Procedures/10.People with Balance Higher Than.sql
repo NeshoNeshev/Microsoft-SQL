364ab4f7617b17c30595b39fc2a56881aacd8cd2
Nesho Neshev <neshev1978@gmail.com>
2021-02-08 12:19:15 +0200

    Update 10.People with Balance Higher Than.sql

--- Functions-and-Stored-Procedures/10.People with Balance Higher Than.sql
+++ Functions-and-Stored-Procedures/10.People with Balance Higher Than.sql
@@ -1,11 +1,12 @@
+
 CREATE PROCEDURE usp_GetHoldersWithBalanceHigherThan(@balance decimal(18, 4))
 AS
-SELECT a.FirstName, a.LastName
-    FROM AccountHolders AS a
-             JOIN Accounts ac ON ah.Id = A.AccountHolderId
-    GROUP BY a.FirstName, a.LastName
-    HAVING SUM(ac.Balance) > @balance
-    ORDER BY a.FirstName, a.LastName
+SELECT ah.FirstName, ah.LastName
+    FROM AccountHolders AS ah
+             JOIN Accounts A ON ah.Id = A.AccountHolderId
+    GROUP BY ah.FirstName, ah.LastName
+    HAVING SUM(A.Balance) > @balance
+    ORDER BY ah.FirstName, ah.LastName
 GO
 
 EXEC usp_GetHoldersWithBalanceHigherThan 15000
