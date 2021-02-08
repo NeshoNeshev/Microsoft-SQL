e60bed56f0cee1a71eb91d591c2e148e3badf7be
Nesho Neshev <neshev1978@gmail.com>
2021-02-08 12:22:32 +0200

    Update 12.Calculating Interest.sql

--- Functions-and-Stored-Procedures/12.Calculating Interest.sql
+++ Functions-and-Stored-Procedures/12.Calculating Interest.sql
@@ -1,8 +1,7 @@
-CREATE PROCEDURE usp_CalculateFutureValueForAccount(@accountId INT, @rate FLOAT)
+CREATE PROCEDURE usp_CalculateFutureValueForAccount(@accountId INT, @interestRate FLOAT)
 AS
-SELECT ah.Id, ah.FirstName, ah.LastName,
-	   FORMAT(a.Balance, 'F2'),
-       dbo.ufn_CalculateFutureValue(a.Balance, @rate, 5)
+SELECT ah.Id, ah.FirstName, ah.LastName, a.Balance,
+       dbo.ufn_CalculateFutureValue(a.Balance, @interestRate, 5)
     FROM AccountHolders AS ah
              JOIN Accounts AS a ON ah.Id = a.AccountHolderId
     WHERE a.Id = @accountId
