8bd304d7bb68faed78dcc7b84d55b67653dccef4
Nesho Neshev <neshev1978@gmail.com>
2021-02-08 12:10:29 +0200

    Update 6.Employees by Salary Level.sql

--- Functions-and-Stored-Procedures/6.Employees by Salary Level.sql
+++ Functions-and-Stored-Procedures/6.Employees by Salary Level.sql
@@ -1,10 +1,10 @@
-CREATE OR ALTER PROCEDURE usp_EmployeesBySalaryLevel(@salryLevel varchar(7))
+
+CREATE PROCEDURE usp_EmployeesBySalaryLevel(@levelOfSalary NVARCHAR(MAX))
 AS
-	BEGIN
-		SELECT FirstName, LastName 
-		FROM Employees
-		WHERE dbo.ufn_GetSalaryLevel(Salary) = @salryLevel
-	END
+SELECT e.FirstName,
+       e.LastName
+    FROM Employees AS e
+    WHERE dbo.ufn_GetSalaryLevel(e.Salary) = @levelOfSalary
 GO
 
-EXEC usp_EmployeesBySalaryLevel 'High'
+EXEC usp_EmployeesBySalaryLevel 'high'
